1f76b345ce0dfd00f13ea6406818a0af9261709b
DantonSoftware <80655836+DantonSoftware@users.noreply.github.com>
2024-08-28 20:43:07 -0400

    actualiza medico

--- Models/DTOs/MedicoDto.cs
+++ Models/DTOs/MedicoDto.cs
@@ -24,7 +24,7 @@ namespace Models.DTOs
         [StringLength(100, MinimumLength = 1, ErrorMessage = "Direccion debe ser Minimo 1 Maximo 100 caracteres")]
         public string Direccion { get; set; }
 
-        [StringLength(40, MinimumLength = 1, ErrorMessage = "Telefono debe ser Minimo 1 Maximo 40 caracteres")]
+        [MaxLength(40)]
         public string Telefono { get; set; }
 
         [Required(ErrorMessage = "Genero es Requerido")]        
